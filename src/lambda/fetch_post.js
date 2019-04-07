f0aa221fcde2df423c75e4aae498b803ede24768
kasthomas <kast@idtechproducts.com>
2019-04-07 14:29:21 -0700

    Update fetch_post.js

--- src/lambda/fetch_post.js
+++ src/lambda/fetch_post.js
@@ -12,7 +12,7 @@ exports.handler = async (event, context) => {
   // When the method is POST, the topic will not be in the event’s
   // queryStringParameters – it’ll be in the event body encoded as a query string
   const params = querystring.parse(event.body);
-  const topic = params.topic || "DNA";
+  // const topic = params.topic || "DNA";
   var myQuery = encodeURI('title:' + params.topic + '&fl=title,id,abstract&wt=json&indent=on');
   var url = API_ENDPOINT + myQuery;
   
