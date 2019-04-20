4eb4b3fe2c7ff19ec18bcd04a3650f91f1f6864a
Kas Thomas <kast@idtechproducts.com>
2019-04-19 22:08:30 -0700

    Update createTDO.js

--- src/lambda/createTDO.js
+++ src/lambda/createTDO.js
@@ -2,6 +2,14 @@
 import querystring from "querystring";
 import fetch from "node-fetch";
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(require("body-parser").json());
+
 var myQuery = `mutation {
   createTDO(
     input: {
