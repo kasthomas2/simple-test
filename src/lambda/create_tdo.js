90799dd295c161ab9b4b5dc926684bc948a8df51
Kas Thomas <kast@idtechproducts.com>
2019-04-19 19:30:24 -0700

    Update create_tdo.js

--- src/lambda/create_tdo.js
+++ src/lambda/create_tdo.js
@@ -35,7 +35,7 @@ exports.handler = async (event, context) => {
   
   var url = API_ENDPOINT;
  
-  return fetch(url, { method: 'POST', headers: {"Authorization": "Bearer " + params.token},  body: "query={me{id}}"  )
+  return fetch(url, { method: 'POST', headers: {"Authorization": "Bearer " + params.token, "Content-Type": "multipart/form-data"},  body: "query={me{id}}"  )
 /*  return fetch(url, { method: 'POST', headers: myHeaders, body: JSON.stringify(myJsonRequest) } ) */
     .then(response => response.json())
     .then(data => ({
