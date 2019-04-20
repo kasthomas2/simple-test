c55153c95e4e7ac89f10945e3fcd8e06722d48a3
Kas Thomas <kast@idtechproducts.com>
2019-04-20 07:20:28 -0700

    Groom the query, handle GET & POST ***

--- src/lambda/createTDO.js
+++ src/lambda/createTDO.js
@@ -1,58 +1,77 @@
-    
 import querystring from "querystring";
 import fetch from "node-fetch";
 
 
 exports.handler = async (event, context) => {
-    
-  var myQuery = `mutation {
-  createTDO(
-    input: {
-      startDateTime: 1548432520,
-      stopDateTime: 1548436341
+
+    var myQuery = `mutation {
+    createTDO(
+      input: {
+        startDateTime: 1548432520,
+        stopDateTime: 1548436341
+      }
+    )
+    {
+      id
+      status
     }
-  )
-  {
-    id
-    status
   }
-}
- `;
+  `;
 
-const API_ENDPOINT = 'https://api.veritone.com/v3/graphql';
+    // GraphQL server endpoint:
+    const API_ENDPOINT = 'https://api.veritone.com/v3/graphql';
 
-  // Only allow incoming POST
-  if (event.httpMethod !== "POST") {
-    return { statusCode: 405, body: "Method Not Allowed " + JSON.stringify(event,null,2) };
-  }
-  if (event.body == "" || !event.body) {
-    return { statusCode: 405, body: "Empty body??? " };
-  }
+    var params = null;
+    var token = null;
 
-  // Since this is a POST, the token will be in the event body as a query string
-  var params = querystring.parse(event.body);
-  
-  var theHeaders = {
-    "Authorization": "Bearer " + params.token, 
-    "Content-Type": "application/json"
-  };
+    //Check for POST
+    if (event.httpMethod == "POST") {
+        params = querystring.parse(event.body);
+        token = params.token;
+    } // Check for GET
+    else if (event.httpMethod == "GET") {
+        params = querystring.parse(event.queryStringParameters);
+        token = params.token;
+    } // Disallow other verbs
+    else {
+        return {
+            statusCode: 405,
+            body: "Method Not Allowed " + JSON.stringify(event, null, 2)
+        };
+    }
 
-    // works
-  var q = 'mutation userLogin { userLogin(input: {userName: "kthomas@veritone.com" password: "xxxxxxx"}) {token}}';
-    
-  var oneLineQuery = myQuery.replace( /\n/g,"" );
-  var queryJSON = { query:  oneLineQuery };
-    
-  return fetch(API_ENDPOINT, { method: 'POST', headers: theHeaders, body: JSON.stringify( queryJSON ) } )
-    .then(response => response.json())
-    .then(data => ({
-      statusCode: 200,
-      headers: { "Content-Type": "text/plain", /* "application/json" */
-                 "Access-Control-Allow-Origin" : "*", /* Required for CORS support to work */
-                 "Access-Control-Allow-Credentials" : true /* Required for cookies, authorization */
-               }, 
-      body: JSON.stringify(data) 
-    }))
-    .catch(error => ({ statusCode: 422, body: String(error) }));
-  
-};
+    var theHeaders = {
+        "Authorization": "Bearer " + params.token,
+        "Content-Type": "application/json"
+    };
+
+    // works if credentials are real
+    // var q = 'mutation userLogin { userLogin(input: {userName: "xxxxxx@veritone.com" password: "xxxxxxx"}) {token}}';
+
+    // groom the query!
+    var oneLineQuery = myQuery.replace(/\n/g, "").replace(/"/g, '\"');
+    var queryJSON = {
+        query: oneLineQuery
+    };
+
+    // hit the server
+    return fetch(API_ENDPOINT, {
+        method: 'POST',
+        headers: theHeaders,
+        body: JSON.stringify(queryJSON)
+    }).then(response => response.json()).then(data => ({
+        statusCode: 200,
+        headers: {
+            "Content-Type": "text/plain",
+            /* "application/json" */
+            "Access-Control-Allow-Origin": "*",
+            /* Required for CORS support to work */
+            "Access-Control-Allow-Credentials": true /* Required for cookies, authorization */
+        },
+        body: JSON.stringify(data)
+    })).catch(error => ({
+        statusCode: 422,
+        body: String(error)
+    }));
+
+}; // end of lambda
