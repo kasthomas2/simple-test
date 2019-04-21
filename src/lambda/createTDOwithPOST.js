89371c6c132e1d89befcaba00d6f731ad9306fd3
Kas Thomas <kast@idtechproducts.com>
2019-04-20 18:03:06 -0700

    debug POST

---
+++ src/lambda/createTDOwithPOST.js
@@ -0,0 +1,60 @@
+import querystring from "querystring";
+import fetch from "node-fetch";
+
+var myQuery = `mutation {
+    createTDO(
+      input: {
+        startDateTime: 1548432520,
+        stopDateTime: 1548436341
+      }
+    )
+    {
+      id
+      status
+    }
+  }
+  `;
+
+// GraphQL server endpoint:
+var API_ENDPOINT = "https://api.veritone.com/v3/graphql";
+
+exports.handler =  function(event, context) {
+
+    // Disallow anything but GET & POST
+    if (event.httpMethod != "GET" && event.httpMethod != "POST") {
+        return { statusCode: 405, body: "Method Not Allowed" };
+    } 
+    
+    var token = event.httpMethod == "POST" ?
+        querystring.parse(event.body).token :
+        event.queryStringParameters.token;
+
+    var theHeaders = {
+        "Authorization": "Bearer " + token,
+        "Content-Type": "application/json"
+    };
+
+    // groom the query!
+    var oneLineQuery = myQuery.replace(/\n/g, "").replace(/"/g, '\"');
+    var queryJSON = {
+        query: oneLineQuery
+    };
+
+    // hit the server
+    return fetch(API_ENDPOINT, {
+        method: 'POST',
+        headers: theHeaders,
+        body: JSON.stringify(queryJSON) + "\n\n" + JSON.stringify(event,null,2)
+    }).then(response => response.json()).then(data => ({
+        statusCode: 200,
+        headers: {
+            "Content-Type": "text/plain",
+            "Access-Control-Allow-Origin": "*", 
+            "Access-Control-Allow-Credentials": true 
+        },
+        body: JSON.stringify(data) 
+    })).catch(error => ({
+        statusCode: 422,
+        body: String(error)
+    }));
+}; // end of lambda
