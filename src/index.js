16795019dfdded6c4dea6c8b40ef938e68cc0317
Kas Thomas <kast@idtechproducts.com>
2019-04-21 20:32:35 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 import "file-loader?name=normalize.css!../node_modules/normalize.css/normalize.css";
 import "file-loader?name=styles.css!./styles.css";
-import "file-loader?name=./scripts/ajax.js!./scripts/ajax.js";
+import "file-loader?name=./scripts/login.js!./scripts/login.js";
