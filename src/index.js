180321f929ef4de01454d5196379a1d983472413
Kas Thomas <kast@idtechproducts.com>
2019-04-27 17:34:21 -0700

    add snackbar.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import "file-loader?name=normalize.css!../node_modules/normalize.css/normalize.css";
 import "file-loader?name=styles.css!./styles.css";
+import "file-loader?name=./scripts/snackbar.js!./scripts/snackbar.js";
 import "file-loader?name=./scripts/login.js!./scripts/login.js";
