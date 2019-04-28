862ef7d07746a39fb98299e8f4d0c8aae6692590
Kas Thomas <kast@idtechproducts.com>
2019-04-27 19:19:19 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
 import "file-loader?name=normalize.css!../node_modules/normalize.css/normalize.css";
 import "file-loader?name=styles.css!./styles.css";
-import "file-loader?name=./scripts/snackbar.js!./scripts/snackbar.js";
-import "file-loader?name=./scripts/login.js!./scripts/login.js";
-import "file-loader?name=./shite/login.js!./scripts/login.js";
+import "file-loader?name=./scripts/utils.js!./scripts/utils.js";
