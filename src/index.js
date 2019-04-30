ec5f7f8f8d53a064168817c633d8ddeb060c6a6e
Kas Thomas <kast@idtechproducts.com>
2019-04-30 08:22:41 -0700

    ./static img

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import "file-loader?name=normalize.css!../node_modules/normalize.css/normalize.css";
 import "file-loader?name=styles.css!./styles.css";
 import "file-loader?name=./scripts/utils.js!./scripts/utils.js";
+import "file-loader?name=./static/slack76.png!./static/slack76.png";
