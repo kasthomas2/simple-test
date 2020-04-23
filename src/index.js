e95904a6a40f5ba3c436b171a795dc38e8ee7f76
Kas Thomas <47995649+kasthomas2@users.noreply.github.com>
2020-04-23 15:08:18 -0700

    Add tx.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "file-loader?name=normalize.css!../node_modules/normalize.css/normalize.css";
 import "file-loader?name=styles.css!./styles.css";
+import "file-loader?name=./scripts/tx.js!./scripts/tx.js";
 import "file-loader?name=./scripts/utils.js!./scripts/utils.js";
 import "file-loader?name=./static/slack76.png!./static/slack76.png";
